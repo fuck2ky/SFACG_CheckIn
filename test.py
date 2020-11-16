b770d96a1cd69caaf13b0ae2a557f3b59c444c33
企鹅2035 <qie2035@qq.com>
2020-11-17 00:30:17 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import urllib2,os
+
+sfacg_data = os.environ["sfacg_data"]
+cookie = os.environ["cookie"]
+auth = os.environ["auth_data"]
+requrl = "https://api.sfacg.com/user/signInfo"
+headerdata = {"Cookie":cookie,"SFSecurity":sfacg_data,"Authorization":auth,"User-Agent":"boluobao/4.6.36(android;22)/OPPO"}
+
+request = urllib2.Request(requrl,None,headerdata)
+request.get_method = lambda: 'PUT'
+response = urllib2.urlopen(request)
+print response.read()
