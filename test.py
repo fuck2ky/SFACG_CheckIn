9b63425be0ad13fb1a28535ba46886056f257e97
企鹅2035 <qie2035@qq.com>
2020-11-17 00:41:32 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import urllib2,os
-
-sfacg_data = os.environ["sfacg_data"]
-cookie = os.environ["cookie"]
-auth = os.environ["auth_data"]
-requrl = "https://api.sfacg.com/user/signInfo"
-headerdata = {"Cookie":cookie,"SFSecurity":sfacg_data,"Authorization":auth,"User-Agent":"boluobao/4.6.36(android;22)/OPPO"}
-
-request = urllib2.Request(requrl,None,headerdata)
-request.get_method = lambda: 'PUT'
-response = urllib2.urlopen(request)
-print response.read()
+try:
+    sfacg_data = os.environ["sfacg_data"]
+    cookie = os.environ["cookie"]
+    auth = os.environ["auth_data"]
+    requrl = "https://api.sfacg.com/user/signInfo"
+    headerdata = {"Cookie":cookie,"SFSecurity":sfacg_data,"Authorization":auth,"User-Agent":"boluobao/4.6.36(android;22)/OPPO"}
+    request = urllib2.Request(requrl,None,headerdata)
+    request.get_method = lambda: 'PUT'
+    response = urllib2.urlopen(request)
+    print response.read()
+except BaseException,Argument:
+    print "error:",Argument
